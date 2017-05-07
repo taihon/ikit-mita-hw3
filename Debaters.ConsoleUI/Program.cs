48833869e7c3359928835d0b9962ed6f01a40fbc
taihon <taihon@users.noreply.github.com>
2017-05-07 21:41:10 +0700

    Changed target framework for solution to .NET 4.6, and finished ConsoleUI

--- Debaters.ConsoleUI/Program.cs
+++ Debaters.ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Debaters.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,28 @@ namespace Debaters.ConsoleUI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Добро пожаловать в спор между Джеем и Тихим Бобом, с участием Числотрона!");
+            int numberCount = -1;
+            while (numberCount <= 0)
+            {
+                Console.Clear();
+                Console.Write("Пожалуйста, введите количество чисел для спора: ");
+                int.TryParse(Console.ReadLine(), out numberCount);
+            }
+            var jay = new Jay();
+            var bob = new SilentBob();
+            var numbertron = new Numbertron();
+            numbertron.NewNumber += jay.FetchNewNumber;
+            numbertron.NewNumber += bob.FetchNewNumber;
+            for (int i = 0; i < numberCount; i++)
+                numbertron.Generate();
+            if(jay.Score > bob.Score)
+                Console.WriteLine(jay.Name);
+            else if (jay.Score < bob.Score)
+                Console.WriteLine(bob.Name);
+            else
+                Console.WriteLine("DRAW");
+            Console.ReadKey();
         }
     }
 }
